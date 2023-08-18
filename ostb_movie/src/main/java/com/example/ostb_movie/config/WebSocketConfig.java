c669d5ac45895862a18d857820ccbd6729248abf
dkdlddbzl <dkdlddbzl@naver.com>
2023-08-18 18:13:39 +0900

    up

--- ostb_movie/src/main/java/com/example/ostb_movie/config/WebSocketConfig.java
+++ ostb_movie/src/main/java/com/example/ostb_movie/config/WebSocketConfig.java
@@ -1,12 +1,10 @@
 package com.example.ostb_movie.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Component
-@Configuration
 public class WebSocketConfig {
 	
 	@Bean
