e49b4e19b2448ae15f4e285a24fece5e36947d94
kimkimkkk <10103gun@naver.com>
2023-08-25 13:19:02 +0900

    up

--- src/main/java/com/example/ostb_movie/config/SecurityConfig.java
+++ src/main/java/com/example/ostb_movie/config/SecurityConfig.java
@@ -52,6 +52,7 @@ public class SecurityConfig {
 				.requestMatchers(mvc.pattern("/book/**")).permitAll()
 				.requestMatchers(mvc.pattern("/movie/**")).permitAll()
 				.requestMatchers(mvc.pattern("/order/**")).permitAll()
+				.requestMatchers(mvc.pattern("/kakao/**")).permitAll()				
 				.requestMatchers(mvc.pattern("/test/**")).hasRole("ADMIN").anyRequest().authenticated())		
 		.formLogin(formLogin -> formLogin //2. 로그인에 관련된 설정
 				.loginPage("/login/loginForm") //로그인 페이지 URL 설정
